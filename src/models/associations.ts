e023c73b90abec7f8230bd10be3db908de8c2615
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-11-28 12:19:48 +0200

    Added player - token associations

--- src/models/associations.ts
+++ src/models/associations.ts
@@ -1,14 +1,19 @@
 import Player from './player.model';
 import Room from './room.model';
+import Token from './token.model';
 
 Room.hasMany(Player, {
   as: Room.includePlayersAlias,
   foreignKey: Room.playerForeignKey,
 });
-
 Player.hasOne(Room, {
   as: Player.includeRoomAlias,
   foreignKey: Player.roomForeignKey,
 });
-
 Room.belongsTo(Player, { foreignKey: Player.roomForeignKey });
+
+Player.hasOne(Token, {
+  as: Player.includeTokenAlias,
+  foreignKey: Player.tokenForeignKey,
+});
+Token.belongsTo(Player, { foreignKey: Player.tokenForeignKey });
--- src/models/player.model.ts
+++ src/models/player.model.ts
@@ -8,17 +8,21 @@ import {
 import sequelize from '../database/db';
 import Room from './room.model';
 import { CommonModel, GetAttrKeysMethod } from './common.model';
+import Token from './token.model';
 
 class Player extends CommonModel<Player> {
   static readonly attributes = ['id', 'name', 'roomId'] as const;
   static readonly roomForeignKey = 'ownerId';
+  static readonly tokenForeignKey = 'playerId';
   static readonly includeRoomAlias = 'room';
+  static readonly includeTokenAlias = 'token';
 
   declare id: CreationOptional<number>;
   declare roomId: ForeignKey<Room['id']>;
   declare name: string;
 
   declare createRoom: HasOneCreateAssociationMixin<Room>;
+  declare createToken: HasOneCreateAssociationMixin<Token>;
 
   declare static getAttrKeys: GetAttrKeysMethod<Player>;
 }
